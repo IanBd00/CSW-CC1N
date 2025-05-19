39829767f9efea726f95f67ff4a48d5b3491d3e8
IanBd00 <166515946+IanBd00@users.noreply.github.com>
2025-05-19 20:39:47 -0300

    Add files via upload

---
+++ AULA 20/script.js
@@ -0,0 +1,11 @@
+function idade(){
+
+    var idadepessoa = document.getElementById("nidade").value;
+    if (idadepessoa >= 1 && idadepessoa <= 11) {
+        window.alert("Você é uma criança");
+    } 
+    else {
+        
+    }
+
+}
